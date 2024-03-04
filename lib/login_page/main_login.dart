fbd4a0446ecb21b08a621a0d7677366c01529395
Meet panchal <panchalmeetr97@gmal.com>
2024-03-04 18:41:36 +0530

    29/2/24 👍

---
+++ lib/login_page/main_login.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+class login_main extends StatelessWidget {
+  const login_main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return main_login();
+  }
+}
+class main_login extends StatefulWidget {
+  const main_login({super.key});
+
+  @override
+  State<main_login> createState() => _main_loginState();
+}
+
+class _main_loginState extends State<main_login> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
